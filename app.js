d88975c32b67248832f5593b0f4260b47c680291
Dipak Dahal <dipakdahalconnects@gmail.com>
2024-04-26 19:25:40 +0545

    recoded logic

--- app.js
+++ app.js
@@ -1,43 +1,78 @@
-const moves=['r','p','s'];
+const moves = ["r", "p", "s"];
+const extended_moves=["Rock","Paper","scissor"];
 
-let images=document.querySelectorAll(".img1 ");
-let value=document.querySelectorAll(".message");
-
-
+let images = document.querySelectorAll(".img1 ");
+let value = document.querySelector(".message");
 
 //console.log(images);
 
 images.forEach((image) => {
-    var user_pick;
-    image.addEventListener("click", function(e){
-        const choose=e.currentTarget.classList;
+  var user_pick;
+  image.addEventListener("click", function (e) {
+    const choose = e.currentTarget.classList;
 
-       // console.log(choose);
+    // console.log(choose);
 
-        if(choose.contains('rock')) var user_pick="r";
+    if (choose.contains("rock")) var user_pick = "r";
+    else if (choose.contains("paper")) var user_pick = "p";
+    else var user_pick = "s";
 
-        else if(choose.contains('paper'))  var user_pick="p";
+    let index = Math.floor(Math.random() * 3);
+    //console.log(index);
 
-        else var user_pick="s";
-                
-       
-        let index=Math.floor(Math.random()*3);
-        console.log(index);
+    computer_pick = moves[index];
 
-        computer_pick=moves[index];
-        //console.log(computer_value);
+    user_pick_move=moves.indexOf(user_pick);
+    computer_pick_move=moves.indexOf(computer_pick);
 
-        switch(true){
-            case user_pick===computer_pick:
-                console.log("thisss")
-                break;
-            
-            default:
-                console.log("uajhsd")
-        }
+    console.log( user_pick_move);
 
+    user_pick_name=extended_moves[user_pick_move];
+    computer_pick_name=extended_moves[computer_pick_move];
+
+
+    console.log(user_pick);
+    console.log(computer_pick);
+    
+
+    joint_moves = user_pick + computer_pick;
+    console.log(joint_moves);
+    switch (joint_moves) {
+      case "rr" :
+      case "ss" :
+      case "pp":
+        value.textContent="Draw!!"
+        break;
+
+        case "rp" :
+        case "ps" :
+        case "sr":
+        value.textContent="you selected *"+user_pick_name +"* computer selected "+computer_pick_name+" computer won!!";
+        
+        break;
+
+        case "rs" :
+        case "pr" :
+        case "sp":
+        value.textContent="you selected "+user_pick_name+" computer selected "+computer_pick_name+ " Congratulations!!you won";
+        
         
 
-    })
-    
+      // case joint_moves == "ps":
+      //   value.textContent="you selected "+user_pick+" computer selected "+computer_pick;
+      //   break;
+
+      // case joint_moves == "pr":
+      //   value.textContent="you selected "+user_pick+" computer selected "+computer_pick;
+      //   break;
+
+      // case joint_moves == "sr":
+      //   value.textContent="you selected "+user_pick+" computer selected "+computer_pick;
+      //   break;
+
+      // case joint_moves == "sp":
+      //   value.textContent="you selected "+user_pick+" computer selected "+computer_pick;
+      //   break;
+    }
+  });
 });
